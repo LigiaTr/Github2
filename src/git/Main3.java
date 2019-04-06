81aaf6c6e25151deb86152e08ba89eb067861323
EduardToth <eduardtoth98@gmail.com>
2019-04-06 18:47:13 +0300

    smth

--- src/git/Main3.java
+++ src/git/Main3.java
@@ -22,12 +22,9 @@ public class Main3 {
 
         return set.size();
     }
-
-
     public static void main(String[] argv)
     {
         System.out.println("Edy nu mai e suparat pe mine ))))) ");
         System.out.println("rebgnthgbtygrtn");
     }
-
 }
