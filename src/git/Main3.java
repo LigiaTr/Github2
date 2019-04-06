b3e33cde320632f4213769dd3263a569594851fe
EduardToth <eduardtoth98@gmail.com>
2019-04-06 18:43:50 +0300

    Vector

--- src/git/Main3.java
+++ src/git/Main3.java
@@ -1,5 +1,5 @@
 package git;
-
+import java.util.Vector;
 import java.util.TreeMap;
 public class Main3 {
 
@@ -8,6 +8,18 @@ public class Main3 {
         TreeMap<Integer, String> tm = new TreeMap<Integer, String>();
 
         tm.put(1, "Illes");
+    }
+
+    public int g()
+    {
+        Vector<Integer> set = new Vector<Integer>();
+
+        set.add( 1 );
+        set.add( 2 );
+        set.add( 3 );
+        set.add( 1 );
+
+        return set.size();
     }
 
 
