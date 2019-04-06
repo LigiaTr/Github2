a65f8e3823cab7eab500a2f2c3493ef4fd63be23
EduardToth <eduardtoth98@gmail.com>
2019-04-06 17:03:48 +0300

    another initial

--- src/git/Main3.java
+++ src/git/Main3.java
@@ -3,7 +3,7 @@ package git;
 public class Main3 {
     public static void main(String[] argv)
     {
-        System.out.println("Edy e suparat pe mine (((((( ");
+        System.out.println("Edy nu mai e suparat pe mine ))))) ");
     }
 
 }
