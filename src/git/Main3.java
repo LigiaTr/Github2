1c08f471b4d5c8e483598c3f4a41d47d56dc78e3
EduardToth <eduardtoth98@gmail.com>
2019-04-06 18:23:37 +0300

    TreeMap

--- src/git/Main3.java
+++ src/git/Main3.java
@@ -1,6 +1,16 @@
 package git;
 
+import java.util.TreeMap;
 public class Main3 {
+
+    public void f()
+    {
+        TreeMap<Integer, String> tm = new TreeMap<Integer, String>();
+
+        tm.put(1, "Illes");
+    }
+
+
     public static void main(String[] argv)
     {
         System.out.println("Edy nu mai e suparat pe mine ))))) ");
